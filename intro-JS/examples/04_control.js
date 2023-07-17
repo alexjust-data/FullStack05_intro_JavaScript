fb2591c57bff0160b84dd703fcef2eac24e3b511
Alex Just <alexjustdata@gmail.com>
2023-07-17 23:01:40 +0200

    ternarios

--- intro-JS/examples/04_control.js
+++ intro-JS/examples/04_control.js
@@ -1,16 +1,37 @@
 
 // ejemplos de if, else, if else, switch
 
-debugger; // paramos la ejecución aquí, pasamos de linea en linea
+//debugger; // paramos la ejecución aquí, pasamos de linea en linea
 let edad = 18;
 
 console.log("Inicio");
 
 if (edad >= 18) {
-    let edad = 1;
+    let edad = 1; // las varibles definidas dentro de las llaves viven sólo ahí dentro
     console.log(`Eres mayor de edad "${edad}" años`); // 1 año
 } else {
     console.log(`No eres mayor de edad`);
 }
 
 console.log("Final", edad);
+
+
+console.log("Inicio2");
+
+if (edad >= 18) {
+    let edad = 1; // las varibles definidas dentro de las llaves viven sólo ahí dentro
+    console.log(`Eres mayor de edad "${edad}" años`); // 1 año
+} else if (14 < edad && edad < 18) {
+    console.log(`Eres adolescente`);
+} else {
+    console.log(`No eres "mayor" de edad`);
+}
+
+console.log("Final2", edad);
+
+
+// ternario
+const resultado = edad === 25; //booleano
+// si esto es verdadero ---  esto es verdad -sino- esto es verdad
+const message = resultado ? "Tienes mi edad" : "No tienes mi edad"
+console.log(message);
