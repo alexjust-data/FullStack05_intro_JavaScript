78e9e0e56f2e6f0417bb2fe9cf2fe3875bb7d6a9
Alex Just <alexjustdata@gmail.com>
2023-07-17 22:52:55 +0200

    iniciando con controladores if else

---
+++ intro-JS/examples/04_control.js
@@ -0,0 +1,16 @@
+
+// ejemplos de if, else, if else, switch
+
+debugger; // paramos la ejecución aquí, pasamos de linea en linea
+let edad = 18;
+
+console.log("Inicio");
+
+if (edad >= 18) {
+    let edad = 1;
+    console.log(`Eres mayor de edad "${edad}" años`); // 1 año
+} else {
+    console.log(`No eres mayor de edad`);
+}
+
+console.log("Final", edad);
