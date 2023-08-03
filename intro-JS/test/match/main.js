ee9cb468090266de928a4c1dd857587063f435c8
Alex Just <alexjustdata@gmail.com>
2023-08-03 19:01:13 +0200

    hecho: final funcionando

--- intro-JS/test/match/main.js
+++ intro-JS/test/match/main.js
@@ -1,32 +1,47 @@
-import { createMatch } from './match.js';
+import { createMatch, getPlayers, playMatch } from './match.js';
 
 
-let players2 = [];
-const players = ['Alberto C', 'David J', 'Javier M', 'Eduardo A'];
 
-for (let i = 0; i < 2; i++) {
-  const randomIndex = Math.floor(Math.random() * players.length);
-  players2.push(players.splice(randomIndex, 1)[0]);
-}
+const options = ['Alberto C', 'David J', 'Javier M', 'Eduardo A'];
+
+// Obtener los primeros dos jugadores aleatorios
+const { players, players2 } = getPlayers(options);
 
 console.log(players2);
 console.log(players);
 
 
-const game = createMatch(players2[0], players2[1]);
+// Crear los partidos utilizando los jugadores seleccionados y los jugadores restantes
+const game1 = createMatch(players2[0], players2[1]);
+const game2 = createMatch(players[0], players[1]);
 
-while (!game.getWinner()) {
-  const randomPoint = Math.floor(Math.random() * 2) + 1;
-  game.pointWonBy(randomPoint);
+let winner1 = playMatch(game1);
+console.log(`Winner 1 : ${winner1}`);
 
-  // líneas para ver la progresión del juego
-  console.log('\n-------------\nCurrentRoundScore : ', game.getCurrentRoundScore());
-  console.log('getMatchScore (rounds) : ', game.getMatchScore());
-  console.log('GameScore (games won) : ', game.getGameScore());
+let winner2 = playMatch(game2);
+console.log(`Winner 2 : ${winner2}`);
 
-}
 
-console.log(`Winner: ${game.getWinner()}`);
+// FINAL
+const final = createMatch(winner1, winner2);
+
+const finalWinner = playMatch(final);
+console.log(`-----------------------`);
+console.log(`FINAL Winner: ${finalWinner}, !!!!!! `);
+console.log(`-----------------------`);
+
+// while (!game1.getWinner()) {
+//   const randomPoint = Math.floor(Math.random() * 2) + 1;
+//   game1.pointWonBy(randomPoint);
+
+//   // líneas para ver la progresión del juego
+//   console.log('\n-------------\nCurrentRoundScore : ', game1.getCurrentRoundScore());
+//   console.log('getMatchScore (rounds) : ', game1.getMatchScore());
+//   console.log('GameScore (games won) : ', game1.getGameScore());
+
+// }
+
+// console.log(`Winner: ${game1.getWinner()}`);
 
 
 
