3b41493c59edfa49c849377ded0610e36c4c24d4
Alex Just <alexjustdata@gmail.com>
2023-08-03 20:10:29 +0200

    hecho: hay ganador para la final

--- intro-JS/test/match/main.js
+++ intro-JS/test/match/main.js
@@ -2,24 +2,28 @@ import { createMatch, getPlayers, playMatch } from './match.js';
 
 
 
+// Jugadores
 const options = ['Alberto C', 'David J', 'Javier M', 'Eduardo A'];
 
-// Obtener los primeros dos jugadores aleatorios
-const { players, players2 } = getPlayers(options);
 
+
+// Obtener jugadores aleatorios
+const { players, players2 } = getPlayers(options);
 console.log(players2);
 console.log(players);
 
 
-// Crear los partidos utilizando los jugadores seleccionados y los jugadores restantes
+
+// SEMI FINALES
 const game1 = createMatch(players2[0], players2[1]);
 const game2 = createMatch(players[0], players[1]);
 
 let winner1 = playMatch(game1);
-console.log(`Winner 1 : ${winner1}`);
+console.log(`Winner semifinal 1 : ${winner1}`);
 
 let winner2 = playMatch(game2);
-console.log(`Winner 2 : ${winner2}`);
+console.log(`Winner semifianl 2 : ${winner2}`);
+
 
 
 // FINAL
@@ -30,18 +34,7 @@ console.log(`-----------------------`);
 console.log(`FINAL Winner: ${finalWinner}, !!!!!! `);
 console.log(`-----------------------`);
 
-// while (!game1.getWinner()) {
-//   const randomPoint = Math.floor(Math.random() * 2) + 1;
-//   game1.pointWonBy(randomPoint);
 
-//   // líneas para ver la progresión del juego
-//   console.log('\n-------------\nCurrentRoundScore : ', game1.getCurrentRoundScore());
-//   console.log('getMatchScore (rounds) : ', game1.getMatchScore());
-//   console.log('GameScore (games won) : ', game1.getGameScore());
-
-// }
-
-// console.log(`Winner: ${game1.getWinner()}`);
 
 
 
